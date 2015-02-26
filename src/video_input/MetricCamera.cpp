92e6ab61c0b5c622bc72a6457a44d3d3c881d0eb
RedMarbles <anoopvargheese@gmail.com>
2015-02-26 13:50:53 +0100

    Corrected previous error with MerticCamera.cpp to use the stream operator instead

--- src/video_input/MetricCamera.cpp
+++ src/video_input/MetricCamera.cpp
@@ -55,7 +55,8 @@ void MetricCamera::compute_helper_matrices()
     Rt.topLeftCorner<3, 3>() = get_calibration().get_pose().rotation;
     Rt.col(3) = get_calibration().get_pose().translation;
 
-    P.topLeftCorner<3, 4>() = K*Rt; // set P the projection matrix
+    P << K*Rt;
+    //P.topLeftCorner<3, 4>() = K*Rt; // set P the projection matrix
     return;
 }
 
